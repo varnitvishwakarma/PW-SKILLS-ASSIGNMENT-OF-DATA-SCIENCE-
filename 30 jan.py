9d6a0f8079682d25a20993afad253a4c2d03381b
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-02-26 00:52:28 +0530

    Create 30 jan.py

---
+++ 30 jan.py
@@ -0,0 +1,53 @@
+
+answer 1
+
+grade=int(input("enter your percentage:"))
+​
+if grade > 90 :
+    print("your are is A")
+elif grade > 80 or grade >=90:
+    print("your garde is B")
+elif grade >60 or grade>=80:
+    print("your grade is c")
+else:
+    print("your grade is D")
+    
+     output:
+enter your percentage: 80
+your grade is c
+
+
+answer 2
+
+price=int(input("enter the price of your bike :"))
+​
+if price >100000:
+    print("road tax is 15%")
+elif price >50000 and price <100000:
+    print("road tax is 10%")
+elif price <= 50000:
+    print("road tax is 5%")
+
+    output:
+    enter the price of your bike : 100
+road tax is 5%
+
+
+
+answer 3
+
+city=input("enter your city")
+​
+if city == "delhi":
+    print("monument = red fort")
+elif city == "agra":
+    print("monument = taj mahal")
+elif city == "jaipur":
+    print("monument = jai mahal")
+else :
+    print("invalid city")
+    
+     output:
+enter your city delhi
+monument = red fort
+
